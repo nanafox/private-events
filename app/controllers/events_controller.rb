c63a50460d2138e91a96c1e44f2804abc29ae7f9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-17 06:08:16 +0000

    feat: Implement controller actions for CRUD operation and user events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,81 @@
 class EventsController < ApplicationController
+  before_action :set_creator, only: [ :user_events ]
+  before_action :set_event, only: [ :show, :edit, :update, :destroy ]
+
+  # GET /events or GET /
   def index
+    @events = Event.includes(:creator).load
+  end
+
+  # GET /events/new
+  def new
+    @event = current_user.events.build
+  end
+
+  # POST /events
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event created successfully"
+    else
+      flash.now[:alert] = "Event creation failed. Fix the errors and try again!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /events/creator/me
+  def current_user_events
+    @events = Event.includes(:creator).where(creator: current_user).load
+  end
+
+  # GET /events/creator/:username
+  def user_events
+    @events = Event.includes(:creator).where(creator: @creator).load
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to current_user_events_path,
+        notice: "Event updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @event.destroy
+      redirect_to current_user_events_path, notice: "Event deleted successfully"
+    else
+      redirect_to current_user_events_path, alert: "Failed to delete event"
+    end
+  end
+
+  private
+
+  # Set the creator for by the given username if found.
+  def set_creator
+    @creator = User.find_by(username: params.expect(:username))
+
+    unless @creator
+      redirect_to root_path, status: :see_other, alert: "User not found!"
+    end
+  end
+
+  def event_params
+    params.expect(event: [ :title, :location, :description, :date ])
+  end
+
+  def set_event
+    @event = Event.find_by(id: params.expect(:id))
+    unless @event
+      redirect_to root_path, status: see_other, alert: "Event not found!"
+    end
   end
 end
