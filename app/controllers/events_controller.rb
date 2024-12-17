82078035845e66535848452077c6e525cef0ee99
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-17 14:32:07 +0000

    feat: Require authentication for event actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ class EventsController < ApplicationController
   before_action :set_creator, only: [ :user_events ]
   before_action :set_event, only: [ :show, :edit, :update, :destroy ]
 
+  before_action :authenticate_user!, except: [ :index ]
+
   # GET /events or GET /
   def index
     @events = Event.includes(:creator).load
