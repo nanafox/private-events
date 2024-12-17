115862ee2f0c8b58b7af73be69e4556559d45e5c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-17 06:19:26 +0000

    feat: Add routes to handle event controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             path: "auth",
+             path_names: {
+               sign_in: "login", sign_out: "logout",
+               password: "secret", confirmation: "verification",
+               unlock: "unblock", sign_up: "register"
+             },
+             controllers: { registrations: "users/registrations" }
+
+  get "events/creator/me", to: "events#current_user_events", as: :current_user_events
+  get "events/creator/:username", to: "events#user_events", as: :user_events
+  resources :events, only: %i[index new create show edit update destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
