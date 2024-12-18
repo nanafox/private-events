10e1f7aca79f1ca75869ea78bd7525a4b09e7073
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-18 18:49:50 +0000

    refactor: Ensure mobile menu JS file is loaded with TurboDrive

--- app/javascript/mobile_ui.js
+++ app/javascript/mobile_ui.js
@@ -1,28 +1,32 @@
-// Mobile menu toggle
-let openMenuButton = document.getElementById("open-menu");
-let closeMenuButton = document.getElementById("close-menu");
-let mobileMenu = document.getElementById("mobile-menu");
+document.addEventListener("turbo:load", () => {
+  // Mobile menu toggle
+  let openMenuButton = document.getElementById("open-menu");
+  let closeMenuButton = document.getElementById("close-menu");
+  let mobileMenu = document.getElementById("mobile-menu");
 
-openMenuButton.addEventListener("click", function () {
-  mobileMenu.classList.remove("hidden");
-  mobileMenu.classList.add("block");
-  openMenuButton.setAttribute("aria-expanded", "true");
-});
+  if (openMenuButton && closeMenuButton && mobileMenu) {
+    openMenuButton.addEventListener("click", () => {
+      mobileMenu.classList.remove("hidden");
+      mobileMenu.classList.add("block");
+      openMenuButton.setAttribute("aria-expanded", "true");
+    });
 
-closeMenuButton.addEventListener("click", function () {
-  mobileMenu.classList.remove("block");
-  mobileMenu.classList.add("hidden");
-  openMenuButton.setAttribute("aria-expanded", "false");
-});
+    closeMenuButton.addEventListener("click", () => {
+      mobileMenu.classList.remove("block");
+      mobileMenu.classList.add("hidden");
+      openMenuButton.setAttribute("aria-expanded", "false");
+    });
 
-// Close the mobile menu when clicking outside
-window.addEventListener("click", function (event) {
-  if (
-    !mobileMenu.contains(event.target) &&
-    !openMenuButton.contains(event.target)
-  ) {
-    mobileMenu.classList.remove("block");
-    mobileMenu.classList.add("hidden");
-    openMenuButton.setAttribute("aria-expanded", "false");
+    // Close the mobile menu when clicking outside
+    window.addEventListener("click", (event) => {
+      if (
+        !mobileMenu.contains(event.target) &&
+        !openMenuButton.contains(event.target)
+      ) {
+        mobileMenu.classList.remove("block");
+        mobileMenu.classList.add("hidden");
+        openMenuButton.setAttribute("aria-expanded", "false");
+      }
+    });
   }
 });
