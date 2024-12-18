36851a4af35cdc86ae7755b0d6a86c1f3be06dc9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-18 18:12:30 +0000

    feat: Add hover effect to navbar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def navbar_classes_for(device_type: "desktop")
     if device_type == "desktop"
-      "text-sm font-semibold text-gray-900"
+      "text-sm font-semibold text-gray-900 hover:text-indigo-900 hover:underline"
     else
       "block px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50"
     end
