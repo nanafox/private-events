e5167e5db83740f87ca89f1c2aab32b3d126ce3e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-19 22:30:42 +0000

    feat: Set caching to Redis in production and in-memory in local environment

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  if Rails.env.local?
+    RATE_LIMIT_STORE = ActiveSupport::Cache::MemoryStore.new
+  else
+    RATE_LIMIT_STORE = ActiveSupport::Cache::RedisCacheStore.new(url: ENV["REDIS_URL"])
+  end
+
   rate_limit to: 3, within: 30.seconds, with: -> {
                redirect_to root_path,
                            alert: "Too many failed login attempts!"
-             }, only: :create
+             }, only: :create, store: RATE_LIMIT_STORE
 end
