944f9b847d9e13ab1d8300e3ce57f85f216128a2
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-06 12:02:19 +0000

    feat: Ensure attendees are also removed when an event is deleted

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
 
   # set up attendances for this event
-  has_many :attendances
+  has_many :attendances, dependent: :destroy
   has_many :attendees, through: :attendances
 
   validates :title, :location, :description, :date, presence: true
